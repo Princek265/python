14376ebb89b6c576f9a0df51140c8c931024c56b
ParikshitDogra <princek26805@gmail.com>
2026-02-03 11:10:28 +0530

    update

--- Numpy/exercise1.py
+++ Numpy/exercise1.py
@@ -1,5 +1,5 @@
 import numpy as np
-# Broadcasting exercises
+# Exercise 1:  Broadcasting exercises
 
 matrix = np.array([[1,2,3],[4,5,6],[7,8,9]])
 vector = np.array([1, 0, -1])
@@ -9,3 +9,18 @@ print("Add: \n", result_add)
 
 result_mul = matrix * 2
 print("Multiplication: \n", result_mul)
+
+# Exercise 2: Generate and Filter a Random Dataset
+
+# Generate random dataset
+dataset = np.random.randint(1, 51, size=(5,5))
+print("Original: \n", dataset)
+
+# Filter values > 25 and replace with 0
+dataset[dataset > 25] = 0
+print("Modified dataset: \n", dataset)
+
+# Calculate summary stats
+print("Sum: ",np.sum(dataset))
+print("Mean: ",np.mean(dataset))
+print("Standart Deviation: ", np.std(dataset))
